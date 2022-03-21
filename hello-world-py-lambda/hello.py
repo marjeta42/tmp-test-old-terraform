731c0f6542047d63d26ebd19b141c7a75da75c86
Marjeta Cedilnik <marjeta.cedilnik@getordermark.com>
2022-03-21 09:20:52 -0700

    python version of the lambda function

---
+++ hello-world-py-lambda/hello.py
@@ -0,0 +1,26 @@
+"""Sample lambda function."""
+import json
+import logging
+
+LOGGING_DEFAULT_LEVEL = logging.DEBUG
+LOGGING_FORMAT = r"%(asctime)s-%(relativeCreated)04d [%(levelname)s] %(name)s " \
+                 r"%(thread)d-%(threadName)s-%(process)d-8s %(pathname)s/%(lineno)s %(message)s"
+
+logging.basicConfig(level=LOGGING_DEFAULT_LEVEL, format=LOGGING_FORMAT)
+logger = logging.getLogger("my-test-handler")
+
+
+def my_test_handler(event):
+    logger.info(f"Event: {event}")
+
+    response_message = "Hello, World"
+
+    return {
+        "statusCode": 200,
+        "headers": {
+            'Content-Type': 'application/json',
+        },
+        "body": json.dumps({
+            "message": response_message,
+        })
+    }
