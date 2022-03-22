b72fd0b3c188b14e6c1ed233edf781263628581d
Marjeta Cedilnik <marjeta.cedilnik@getordermark.com>
2022-03-22 10:26:47 -0700

    moving state to s3

--- main.tf
+++ main.tf
@@ -14,6 +14,13 @@ terraform {
     }
   }
 
+  backend "s3" {
+    bucket = "om-terraform-remote-state-storage-bucket"
+    key    = "tmp-learn-terraform-functions/terraform.tfstate"
+#    workspace_key_prefix = "tmp-learn-terraform-functions"
+    region = "us-east-2"
+  }
+
   required_version = "~> 1.0"
 }
 
@@ -77,7 +84,7 @@ resource "aws_cloudwatch_log_group" "hello_world" {
 }
 
 resource "aws_iam_role" "lambda_exec" {
-  name = "serverless_lambda_hello"
+  name = "serverless_lambda_hello2"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -97,3 +104,68 @@ resource "aws_iam_role_policy_attachment" "lambda_policy" {
   role       = aws_iam_role.lambda_exec.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
+
+# -------------------------------------------------------------------
+# configure an API Gateway
+# -------------------------------------------------------------------
+
+#resource "aws_apigatewayv2_api" "lambda" {
+#  name          = "serverless_lambda_hello_gw"
+#  protocol_type = "HTTP"
+#}
+#
+#resource "aws_apigatewayv2_stage" "lambda" {
+#  api_id = aws_apigatewayv2_api.lambda.id
+#
+#  name        = "serverless_lambda_hello_stage"
+#  auto_deploy = true
+#
+#  access_log_settings {
+#    destination_arn = aws_cloudwatch_log_group.api_gw.arn
+#
+#    format = jsonencode({
+#      requestId               = "$context.requestId"
+#      sourceIp                = "$context.identity.sourceIp"
+#      requestTime             = "$context.requestTime"
+#      protocol                = "$context.protocol"
+#      httpMethod              = "$context.httpMethod"
+#      resourcePath            = "$context.resourcePath"
+#      routeKey                = "$context.routeKey"
+#      status                  = "$context.status"
+#      responseLength          = "$context.responseLength"
+#      integrationErrorMessage = "$context.integrationErrorMessage"
+#      }
+#    )
+#  }
+#}
+#
+#resource "aws_apigatewayv2_integration" "hello_world" {
+#  api_id = aws_apigatewayv2_api.lambda.id
+#
+#  integration_uri    = aws_lambda_function.hello_world.invoke_arn
+#  integration_type   = "AWS_PROXY"
+#  integration_method = "POST"
+#}
+#
+#resource "aws_apigatewayv2_route" "hello_world" {
+#  api_id = aws_apigatewayv2_api.lambda.id
+#
+#  route_key = "GET /hello"
+#  target    = "integrations/${aws_apigatewayv2_integration.hello_world.id}"
+#}
+#
+#resource "aws_cloudwatch_log_group" "api_gw" {
+#  name = "/aws/api_gw/${aws_apigatewayv2_api.lambda.name}"
+#
+#  retention_in_days = 30
+#}
+#
+#resource "aws_lambda_permission" "api_gw" {
+#  statement_id  = "AllowExecutionFromAPIGateway"
+#  action        = "lambda:InvokeFunction"
+#  function_name = aws_lambda_function.hello_world.function_name
+#  principal     = "apigateway.amazonaws.com"
+#
+#  source_arn = "${aws_apigatewayv2_api.lambda.execution_arn}/*/*"
+#}
+#
